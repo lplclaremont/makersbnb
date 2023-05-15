895da43e1265e771575695ad0c5ed2f24108dcbe
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-05-15 12:45:36 +0100

    add method test driven

--- lib/listing_repository.rb
+++ lib/listing_repository.rb
@@ -1,5 +1,37 @@
 require_relative './listing'
+require_relative './database_connection'
 
 class ListingRepository
+  def add(listing)
+    sql = 'INSERT INTO listings 
+    (listing_name, listing_description, price, user_id)
+    VALUES ($1, $2, $3, $4)'
+    params = [
+      listing.listing_name,
+      listing.listing_description,
+      listing.price,
+      listing.user_id
+    ]
 
+    DatabaseConnection.exec_params(sql, params)
+    return nil
+  end
+
+  def all
+    sql = 'SELECT * FROM listings;'
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    listings = []
+    result_set.each do |record|
+      listing = Listing.new
+      listing.id = record['id'].to_i
+      listing.listing_name = record['listing_name']
+      listing.listing_description = record['listing_description']
+      listing.price = record['price'].to_i
+      listing.user_id = record['user_id'].to_i
+
+      listings << listing
+    end
+    return listings
+  end
 end
