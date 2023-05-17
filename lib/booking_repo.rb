ac563529ccc2cd8beeb76b405770abe4e66dab46
Charlie Parker <cshjp@MASTUDENT-Charlie-Parker-C02G4FQVQ6L4.local>
2023-05-17 14:59:19 +0100

    wrote a failing test for find_requests_by_listing_id

--- lib/booking_repo.rb
+++ lib/booking_repo.rb
@@ -1,5 +1,9 @@
 require_relative 'booking'
 
 class BookingRepo
+
+  def find_requests_by_listing_id(listing_id)
+
+  end
   
 end
--- spec/booking_repo_spec.rb
+++ spec/booking_repo_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe BookingRepo do
     reset_tables
   end
 
+  context 'using the listing id' do
+    it 'finds multiple requests' do
+      repo = BookingRepo.new
+      results = repo.find_requests_by_listing_id(1)
+      expect(results.length).to eq 2 
+      expect(results.first.booking_user_name).to eq 'Donkey'
+      expect(results.first.date).to eq '2023-05-12'
+      expect(results.last.booking_user_name).to eq 'Fiona'
+      expect(results.last.date).to eq '2023-05-12'
+    end
+
+    xit 'finds no requests when listing doesnt have any' do
+
+    end
+
+    xit 'it fails when the listing doesnt exist' do
+
+    end
+  end
 end
