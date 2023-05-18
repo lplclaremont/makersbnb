65599d1c6d0e10f7a766e8dcb66965ca04ce4002
Barney Wilton <barney@Barneys-MBP.home>
2023-05-18 17:09:27 +0100

    Moved initial mail set up into a class

--- spec/mailer_spec.rb
+++ spec/mailer_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Mailer do
   context 'send' do
     it 'sends an email' do
       mailer = Mailer.new
-      expect(mailer.send).to eq 'test'
+      expect(mailer.send).to eq 'Email sent'
     end
   end
 end
