dec5a88871697c097a2612594ac32e0975cb9766
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-05-18 10:15:02 +0100

    gitignore made

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require 'simplecov'
 require 'simplecov-console'
 require 'database_connection'
 
+SimpleCov.add_filter 'coverage/'
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
